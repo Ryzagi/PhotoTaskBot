6fd41bf3264e970cedfc2e7790ec4821ce78b126
ryzagi <ryzagi@livexp.com>
2024-11-07 13:07:40 +0200

    feat: add new endpoint get existing solution and change prompt

--- bot/constants.py
+++ bot/constants.py
@@ -3,13 +3,14 @@ GPT_MODEL = "gpt-4o"
 DOWNLOAD_ENDPOINT = "/tasker/api/download_image"
 SOLVE_ENDPOINT = "/tasker/api/solve_task"
 ADD_NEW_USER_ENDPOINT = "/tasker/api/add_new_user"
+GET_EXIST_SOLUTION_ENDPOINT = "/tasker/api/get_exist_solution"
 
 SUB_FOLDER = "/task_images/"
 DEFAULT_DAILY_LIMIT = 1
 TASK_HELPER_PROMPT_TEMPLATE_SYSTEM = "You are given an image of a math problem. Help the user solve it."
 
 
-TASK_HELPER_PROMPT_TEMPLATE_USER = """    
+_TASK_HELPER_PROMPT_TEMPLATE_USER = """    
 Return the solutions in language of tasks for the following problems in json format.
 Responds in Markdown format.
 Output the solutions in the following JSON format:
@@ -32,6 +33,36 @@ Output the solutions in the following JSON format:
                     "step_2",
                     ...
                 ]
+            },
+            ...
+        ]
+    }
+"""
+
+TASK_HELPER_PROMPT_TEMPLATE_USER = """    
+Return the solutions in language of tasks for the following problems in json format.
+Middle dot (·) is used to product two numbers.
+Responds in Markdown format.
+Output the solutions in the following JSON format:
+    {
+        "solutions": [
+            {
+                "problem": "problem_1",
+                "steps": [
+                    "step_1",
+                    "step_2",
+                    ...
+                ],
+                "solution": "solution_1",
+            },
+            {
+                "problem": "problem_2",
+                "steps": [
+                    "step_1",
+                    "step_2",
+                    ...
+                ],
+                "solution": "solution_2",
             },
             ...
         ]
