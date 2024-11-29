74befd0b7af3a317947e617261ca21825c4805d1
ryzagi <ryzagi@livexp.com>
2024-11-29 08:29:57 +0200

    fix: change tg admin contant

--- bot/app/routers.py
+++ bot/app/routers.py
@@ -103,7 +103,8 @@ async def cmd_refund(
 ):
     transaction_id = command.args
     user_id = message.from_user.id
-    if user_id != os.getenv("ADMIN_ID"):
+    print(os.getenv("ADMIN_TG_ID"))
+    if user_id != os.getenv("ADMIN_TG_ID"):
         await message.answer(
             l10n.format_value("refund-not-allowed")
         )
